ce792aebce39bf419a4e2c387c25b54bb37201b3
AdityaSharma484 <91065144+AdityaSharma484@users.noreply.github.com>
2022-10-05 13:20:59 +0530

    Create StarPatternUsingJava.java

---
+++ JAVA PROGRAM/StarPatternUsingJava.java
@@ -0,0 +1,31 @@
+/* WRITE A JAVA PROGRAM TO MAKE THE FOLLOWING PATTERN:
+*
+**
+***
+****
+*****
+*/
+import java.util.*;
+class Pattern
+{
+    public static void main(String args[])
+    {
+        for(int i = 1; i <= 5; i++)
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+OUTPUT:
+*
+**
+***
+****
+*****
+*/
